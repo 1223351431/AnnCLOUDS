39ac892bb1898527690ad3a4c0c89248519e6227
1223351431 <1223351431@qq.com>
2023-09-07 12:16:38 +0800

    自动推送

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global http.sslverify false
 git config --global user.name '1223351431'
 git config --global user.email '1223351431@qq.com'
 git add .
-git commit -m $1
+git commit -m '自动推送'
 git push
